27a7c33308b2852d83962a0991487a47fdc3bce0
Marcin Pasiewicz <mpasiewicz@getresponse.com>
2017-11-28 17:08:24 +0100

    catch of the day application done

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyB8EGvY9PjAL2_n8sY5jGWX1RsqYobr030",
+  authDomain: "catch-of-the-day-74880.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-74880.firebaseio.com",
+});
+
+export default base;
---
+++ src/components/NotFound.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFound = () => {
+  return <h2>Not Found!111!</h2>
+}
+
+export default NotFound;
